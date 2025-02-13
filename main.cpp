45bf6afcd4bb8fa20a8e0855c7ffe77385b69560
s.sayfiyev <s.sayfiyev@newuu.uz>
2025-02-13 12:32:15 +0500

    lab5

---
+++ main.cpp
@@ -0,0 +1,208 @@
+#include <iostream>
+using namespace std;
+int main() {
+    /*switch problem
+    int a,b,c,result;
+    cout<<"Enter two numbers: ";
+    cin>>a>>b;
+    cout<<"Please select number from 1 to 4 according to:\n 1 for addition\n 2 for substraction\n 3 for multiplication\n 4 for division:  ";
+    cin>>c;
+    switch (c) {
+        case 1:
+            result=a+b;
+        break;
+        case 2:
+            result=a-b;
+        break;
+        case 3:
+            result=a*b;
+        break;
+        case 4:
+            result=a/b;
+        break;
+    }
+    cout<<result;
+    /*problem 1.1
+    for(int i=1; i<=10; i++){
+        cout<<i<<" ";
+    }*/
+    /*problem 1.2
+    int n;
+    cin>>n;
+    for(int i=1;i<=n; i++){
+        cout<<i<<" ";
+    }*/
+
+    /*problem 1.3
+
+    int sum=0;
+   for(int i=1; i<=10; i++){
+
+       sum=sum+i;
+   }
+   cout<<sum;*/
+    /*problem 1.4
+    int n;
+    cin>>n;
+     int sum=0;
+    for(int i=1; i<=n; i++){
+
+        sum=sum+i;
+    }
+    cout<<sum;*/
+    /*problem 1.5
+     float sum=0;
+     float avarage;
+    for(int i=1; i<=10; i++){
+
+        sum=sum+i;
+    }
+    avarage= sum/10;
+    cout<<avarage;*/
+    /*problem 1.6
+    int n;
+    cin>>n;
+     float sum=0;
+     float avarage;
+    for(int i=1; i<=n; i++){
+
+        sum=sum+i;
+    }
+    avarage= sum/n;
+    cout<<avarage;*/
+    /*problem 2.1
+    for(int i=1; i<=10; i=i+2){
+        cout<<i<<" ";
+    }*/
+    /*problem 2.2
+    for(int i=2; i<=10; i=i+2){
+        cout<<i<<" ";
+    }*/
+    /* problem 2.3
+    int n;
+    cin>>n;
+    for(int i=1; i<=n; i=i+2){
+        cout<<i<<" ";
+    }*/
+    /*problem 2.4
+    int n;
+    cin>>n;
+    for(int i=2; i<=n; i=i+2){
+        cout<<i<<" ";
+    }*/
+    /*problem 2.5
+    for(int i=1; i<=100; i++){
+        if (i%10==0){
+            cout<<i<<" ";
+        }
+    }*/
+    /*problem 2.6
+    int n;
+    cin>>n;
+    for(int i=1; i<=n; i++){
+        if (i%10==0){
+            cout<<i<<" ";
+        }
+    }
+    problem 3
+    for(int i = 65; i <= 90; i ++){
+        char n = i;
+        cout << n << " ";
+        if((i + 1) % 5 == 0) cout << endl;
+
+    }*/
+    /*problem 4
+    int n, factoral;
+    cin>>n;
+    factoral=1;
+    for (int i=1;i<=n; i++){
+        factoral=factoral*i;
+    }
+    cout<<factoral;*/
+    //problem 5
+    /*float gpa,total_gpa,classes, credits, total_credits;
+    cin>>classes;
+    float sum=0;
+    total_credits=0;
+    for (int i=1;i<=3;i++){
+        cin>>credits>>gpa;
+        sum=sum+credits*gpa;
+        total_credits=total_credits+credits;
+    }
+
+    total_gpa=sum/total_credits;
+    cout<<total_gpa;*/
+    /*problem 6
+    float gpa,total_gpa,classes, credits, total_credits,n,k;
+    cin>>n;
+    k=0;
+    for(int i=1; i<=n;i++){
+        cin>>classes;
+        float sum=0;
+        total_credits=0;
+        for (int i=1;i<=classes;i++){
+            cin>>credits>>gpa;
+            sum=sum+credits*gpa;
+            total_credits=total_credits+credits;
+        }
+
+        total_gpa=sum/total_credits;
+        k=k+total_gpa;
+    }
+    cout<<k/n;*/
+    /*problem7
+    int a;
+    cin >> a;
+    for (int j = 0; j < a; j++) {
+        cout<<endl;
+
+        for (int i = 0; i < a; i++) {
+            cout<<"*";
+
+        }
+    }*/
+    /*problem8
+    int a;
+    cin >> a;
+    for (int i = 0; i < a; i++) {
+        for (int j = 0; j <= i; j++) {
+            cout << "*";
+        }
+        cout << endl;
+
+    }*/
+    /*int a, b=0;
+    cin >> a;
+    if (a<2) {
+        cout<<"Number is not prime";
+    }
+    else
+        for (int i = 2; i < a; i++) {
+            if (a % i == 0) {
+                b=1;
+            }
+
+        }
+        if (b==1) {
+            cout<<"Number is not prime";
+
+        }
+        else
+            cout<<"Number is  prime";*/
+    /*problem 10
+    int a,b=0;
+    cin >> a;
+    for (int i = 1; i <= a; i++) {
+        b=b+i*i;
+    }
+    cout << b;*/
+
+    int a,b=0;
+    cin>>a;
+    for (int i=10,i<=)
+
+
+
+
+    return 0;
+}
